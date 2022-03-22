0bf0207ea1b48c791fab18f3784fe8eff1189b81
juan <tipito4085@gmail.com>
2022-03-22 17:21:42 -0500

    Update

--- reserves.js
+++ reserves.js
@@ -1,4 +1,4 @@
-let contenedor = document.getElementById("contenedor")
+let contenedor = document.getElementById("contenedor");
 contenedor.addEventListener("click", function (event) {
 
 
@@ -7,4 +7,48 @@ contenedor.addEventListener("click", function (event) {
         event.target.src = "img/cinema2.png"
 
     }
-})
+});
+let etiquetaBoton = document.getElementById("button");
+etiquetaBoton.addEventListener("click", escuchar);
+function escuchar(event) {
+    event.preventDefault();
+    console.log("Clicked")
+    let pelicula = document.getElementById("selectPelicula").value
+    let asiento = document.getElementById("asiento").value
+    let cantidad = document.getElementById("cantidad").value
+    let age = document.getElementById("edad").value
+    console.log("Película: " + pelicula + ", asiento: " + asiento + ", cantidad: " + cantidad + ", edad: " + age);
+
+    if (age < 15) {
+        if (pelicula == "padrino") {
+            alert("Esta película es exclusiva para mayores de 15 años");
+        }
+        else if (pelicula == "licoricePizza") {
+            alert("Esta película es exclusiva para mayores de 15 años");
+        }
+
+    } else {
+        let preferencial = 20000;
+        let estandar = 12000;
+        if (pelicula == "batman") {}
+        if (asiento == "preferencial") {
+            let ressult = cantidad * preferencial
+            alert("Su total es: " + ressult)
+        }
+        else if (asiento == estandar); {
+            let ressult = cantidad * estandar
+        };
+    }
+
+}
+
+  // if(age < 15) {
+    //   if(pelicula == "padrino"){
+      //      alert("Esta película es exclusiva para mayores de 15 años");
+        //}
+        //else if (pelicula == "licoricePizza") {
+          //  alert("Esta película es exclusiva para mayores de 15 años");
+        //}
+    //}
+//}
+
