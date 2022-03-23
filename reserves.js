630270ee6aa315c09208838bbb690abfe3891daa
juan <tipito4085gmail.com>
2022-03-23 15:57:36 -0500

    Update

--- reserves.js
+++ reserves.js
@@ -1,11 +1,22 @@
 let contenedor = document.getElementById("contenedor");
+let flag = true;
+
 contenedor.addEventListener("click", function (event) {
 
+    if (flag == true) {
 
-    if (event.target.classList.contains("img-fluid")) {
+        if (event.target.classList.contains("img-fluid")) {
 
-        event.target.src = "img/cinema2.png"
+            event.target.src = "img/cinema2.png"
+            flag = false
+        }
+    }
+    else {
+        if (event.target.classList.contains("img-fluid")) {
 
+            event.target.src = "img/cinema-chair.png"
+            flag = true
+        }
     }
 });
 let etiquetaBoton = document.getElementById("button");
@@ -20,35 +31,40 @@ function escuchar(event) {
     console.log("Película: " + pelicula + ", asiento: " + asiento + ", cantidad: " + cantidad + ", edad: " + age);
 
     if (age < 15) {
+        console.log("menor")
         if (pelicula == "padrino") {
             alert("Esta película es exclusiva para mayores de 15 años");
         }
         else if (pelicula == "licoricePizza") {
             alert("Esta película es exclusiva para mayores de 15 años");
+        } else if (pelicula == "batman" || pelicula == "uncharted" || pelicula == "princesa" || pelicula == "justiciero" || pelicula == "belfast" || pelicula == "libroAmor" || pelicula == "callejonAlmas" || pelicula == "swordArt") {
+            let preferencial = 20000;
+            let estandar = 12000;
+            console.log(asiento)
+            if (asiento == "preferencial") {
+                let ressult = cantidad * preferencial
+                console.log("entre aqui")
+                alert("Su total es: " + ressult)
+            }
+            else {
+                let ressult = cantidad * estandar
+                console.log("entre a estandar")
+                alert("Su total es de: " + ressult)
+            };
         }
-
     } else {
+        console.log("mayor")
         let preferencial = 20000;
         let estandar = 12000;
-        if (pelicula == "batman") {}
         if (asiento == "preferencial") {
             let ressult = cantidad * preferencial
             alert("Su total es: " + ressult)
         }
-        else if (asiento == estandar); {
+        else {
             let ressult = cantidad * estandar
+            alert("Su total es de: " + ressult)
         };
     }
 
 }
-
-  // if(age < 15) {
-    //   if(pelicula == "padrino"){
-      //      alert("Esta película es exclusiva para mayores de 15 años");
-        //}
-        //else if (pelicula == "licoricePizza") {
-          //  alert("Esta película es exclusiva para mayores de 15 años");
-        //}
-    //}
-//}
 
